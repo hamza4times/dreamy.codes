e90793614d6ee7d17b4c163eb8b7018ff06bac1d
hamza4times <241655835+hamza4times@users.noreply.github.com>
2026-06-17 17:06:57 -0500

    add models

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ createEditor();
 
 const OPENROUTER_API_KEY = import.meta.env.VITE_OPENROUTER_API_KEY;
 
-
 /* AI Function */
 export async function askAI(prompt) {
+  const selectedModel = document.getElementById("model-select").value;
   const response = await fetch(
     "https://openrouter.ai/api/v1/chat/completions",
     {
@@ -18,7 +18,7 @@ export async function askAI(prompt) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "openai/gpt-oss-20b:free",
+        model: selectedModel,
         max_tokens: 512,
         messages: [
           {
